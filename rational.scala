e17777e544f3c0f940523410c768cd039ae6c560
Nimish Gupta <nimish28@gmail.com>
2014-01-04 21:21:00 -0500

    explicit fields for numerator and denominator added and add method added

--- rational.scala
+++ rational.scala
@@ -6,8 +6,14 @@ class Rational (n : Int, d : Int) {
   /* add a precondition that denominator is not 0 */
   require (d != 0)
 
+  val numer = n
+  val denom = d
+
   /* inherits the implmentation of toString defined in class java.lang.Object
    * overriding with new impl
    */
   override def toString = n + "/" + d
+
+  def add (that : Rational) : Rational = 
+    new Rational (numer * that.denom + that.numer * denom, denom * that.denom)
 }
