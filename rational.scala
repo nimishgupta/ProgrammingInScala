3e2115a8921b11cb67ca34ae683ec3d7e4a196c4
Nimish Gupta <nimish28@gmail.com>
2014-01-04 21:44:34 -0500

    Aux constructor + few methods

--- rational.scala
+++ rational.scala
@@ -1,19 +1,32 @@
 
 /* n and d are class parameters
- * Scala will automatically conjure up a primary constructor that takes these two parameters from the boilerplate 
  */
 class Rational (n : Int, d : Int) {
+  /* Any free code goes into the primary constructor */
+
   /* add a precondition that denominator is not 0 */
   require (d != 0)
 
+  /* Explicit fields, required to access similar fields of objects
+   * other than "this"
+   */
   val numer = n
   val denom = d
+
+  /* auxiliary constructor */
+  def this (n : Int) = this (n, 1)
 
   /* inherits the implmentation of toString defined in class java.lang.Object
    * overriding with new impl
    */
   override def toString = n + "/" + d
 
-  def add (that : Rational) : Rational = 
+  def add (that: Rational) : Rational = 
     new Rational (numer * that.denom + that.numer * denom, denom * that.denom)
+
+  def lessthan (that: Rational) =
+    this.numer * that.denom < this.denom * that.numer
+
+  def max (that: Rational) =
+    if (this lessthan that) that else this
 }
